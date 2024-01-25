9f83a873f90e08203df6c9842e510069c9694bd5
qjerome <qjerome@rawsec.lu>
2024-01-25 15:56:36 +0100

    fix file_unlink probe reporting bpf errors in very specific conditions
    Signed-off-by: qjerome <qjerome@rawsec.lu>

--- kunai-common/src/macros.rs
+++ kunai-common/src/macros.rs
@@ -1,10 +1,14 @@
 #[macro_export]
+/// inspect_err(expr, closure)
+/// calls closure if the result of expr is an Err
 macro_rules! inspect_err {
-    ($e:expr, $clos:expr) => {
-        if let Err(e) = $e {
+    ($e:expr, $clos:expr) => {{
+        let res = $e;
+        if let Err(e) = res.as_ref() {
             $clos(e);
         }
-    };
+        res
+    }};
 }
 
 #[macro_export]
