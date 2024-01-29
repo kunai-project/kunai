9d7abf473660cb2059b569ea952da6785df6c4df
qjerome <qjerome@rawsec.lu>
2024-01-29 16:05:19 +0100

    fix #35 bug in schedule probe
    Signed-off-by: qjerome <qjerome@rawsec.lu>

--- kunai-common/src/co_re/core_mm_struct.rs
+++ kunai-common/src/co_re/core_mm_struct.rs
@@ -8,8 +8,17 @@ impl mm_struct {
     rust_shim_kernel_impl!(mm_struct, arg_start, u64);
 
     #[inline(always)]
+    // inspired from: https://elixir.bootlin.com/linux/v6.6.13/source/fs/proc/base.c#L256
     pub unsafe fn arg_len(&self) -> Option<u64> {
-        Some(self.arg_end()? - self.arg_start()?)
+        let start = self.arg_start()?;
+        let end = self.arg_end()?;
+        Some({
+            if end == 0 || start >= end {
+                0
+            } else {
+                end - start
+            }
+        })
     }
 
     rust_shim_kernel_impl!(mm_struct, arg_end, u64);
