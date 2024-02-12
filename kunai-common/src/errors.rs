0ca88341601fe041869d5651f4b899889374d601
qjerome <qjerome@rawsec.lu>
2024-02-12 10:24:19 +0100

    new FileNotFound ProbeError
    Signed-off-by: qjerome <qjerome@rawsec.lu>

--- kunai-common/src/errors.rs
+++ kunai-common/src/errors.rs
@@ -63,6 +63,8 @@ pub enum ProbeError {
     KProbeArgFailure,
     #[error("unexpected null pointer")]
     NullPointer,
+    #[error("file not found")]
+    FileNotFound,
     #[wrap]
     BpfMapError(MapError),
     #[wrap]
