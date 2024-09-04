f200b7932a357ec1877345c7afa1287153e01ea5
qjerome <qjerome@rawsec.lu>
2024-09-04 21:30:40 +0200

    add:[xtask] handle build profile for userland

--- xtask/src/user.rs
+++ xtask/src/user.rs
@@ -1,4 +1,4 @@
-use std::{os::unix::process::CommandExt, process::Command};
+use std::{borrow::Cow, os::unix::process::CommandExt, process::Command};
 
 use anyhow::Context as _;
 use clap::Parser;
@@ -11,6 +11,9 @@ pub struct RunOptions {
     /// Build and run the release target
     #[clap(long)]
     pub release: bool,
+    /// Build and run profile
+    #[clap(long)]
+    pub profile: Option<String>,
     /// Specify the building target for userland
     #[clap(default_value = "x86_64-unknown-linux-gnu", long)]
     pub target: String,
@@ -41,6 +44,7 @@ impl From<&RunOptions> for BuildOptions {
     fn from(value: &RunOptions) -> Self {
         Self {
             target: value.target.clone(),
+            profile: value.profile.clone(),
             linker: None,
             bpf_target: value.bpf_target,
             bpf_linker: value.bpf_linker.clone(),
@@ -53,9 +57,12 @@ impl From<&RunOptions> for BuildOptions {
 
 #[derive(Debug, Parser, Clone)]
 pub struct BuildOptions {
-    /// Build and run the release target
+    /// Build the release target
     #[clap(long)]
     pub release: bool,
+    /// Build profile
+    #[clap(long)]
+    pub profile: Option<String>,
     /// Specify the building target for userland
     #[clap(default_value = "x86_64-unknown-linux-gnu", long)]
     pub target: String,
@@ -103,9 +110,15 @@ impl From<&BuildOptions> for ebpf::BuildOptions {
 }
 
 fn cargo(command: &str, opts: &BuildOptions) -> Result<(), anyhow::Error> {
-    let mut args = vec![command];
+    let mut opts = opts.clone();
+    let mut args = vec![command.to_string()];
+
     if opts.release {
-        args.push("--release")
+        opts.profile = Some("release".into());
+    }
+
+    if let Some(profile) = opts.profile.as_ref() {
+        args.push(format!("--profile={}", profile));
     }
 
     let mut rustflags = vec_rustflags()?;
@@ -114,15 +127,13 @@ fn cargo(command: &str, opts: &BuildOptions) -> Result<(), anyhow::Error> {
         rustflags.push(format!("-C linker={linker}"))
     }
 
-    let target = format!("--target={}", opts.target);
-    args.push(&target);
+    args.push(format!("--target={}", opts.target));
 
-    opts.build_args.iter().for_each(|ba| args.push(ba));
+    opts.build_args.iter().for_each(|ba| args.push(ba.clone()));
 
     let mut cmd = Command::new("cargo");
     if !rustflags.is_empty() {
         cmd.env("RUSTFLAGS", rustflags.join(" "));
-        println!("setting up rustflag")
     }
 
     let status = cmd.args(&args).status().expect("failed to build userspace");
@@ -152,7 +163,10 @@ pub fn run(ebpf_dir: &str, opts: &RunOptions) -> Result<(), anyhow::Error> {
     build_all(ebpf_dir, &opts.into())?;
 
     // profile we are building (release or debug)
-    let profile = if opts.release { "release" } else { "debug" };
+    let profile = match opts.profile.as_ref() {
+        Some(profile) => Cow::Borrowed(profile.as_str()),
+        _ => "debug".into(),
+    };
 
     // we get the binary path
     let bin_path = format!("target/{}/{profile}/kunai", opts.target);
