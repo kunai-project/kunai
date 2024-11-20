145ac5666f7dc09c23537748f3e0742a0b7704f0
qjerome <qjerome@rawsec.lu>
2024-11-20 13:20:48 +0100

    fix(common): double quotes on spaces in Buffer::to_argv

--- kunai-common/src/buffer/user.rs
+++ kunai-common/src/buffer/user.rs
@@ -3,16 +3,27 @@ use core::str;
 use super::Buffer;
 
 impl<const N: usize> Buffer<N> {
+    #[inline]
     pub fn to_command_line(&self) -> String {
         self.to_argv().join(" ")
     }
 
+    #[inline]
     pub fn to_argv(&self) -> Vec<String> {
         self.as_slice()
             .split(|&b| b == b'\0')
             // this must not panic as we are sure to have utf8 from kernel
             .map(|s| str::from_utf8(s).unwrap().to_string())
             .filter(|s| !s.is_empty())
+            .map(|s| {
+                if s.chars().any(|c| c.is_whitespace()) {
+                    // we wrap strings containg space between double quotes
+                    // but we also need to replace double quotes by escaped double quotes
+                    format!(r#""{}""#, s.replace(r#"""#, r#"\""#))
+                } else {
+                    s
+                }
+            })
             .collect()
     }
 }
