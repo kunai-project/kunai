a664f7c76934b0e0791964bcf1518ec0e0e95b9d
Quentin JEROME <qjerome@rawsec.lu>
2025-02-19 10:46:32 +0100

    create: xlaunch cross arch xtask launcher (#184)
    * create: xlaunch cross arch xtask launcher
    
    * fix: cargo config.toml files

---
+++ xlaunch/src/main.rs
@@ -0,0 +1,26 @@
+use std::{env, error::Error};
+
+// we execute cargo run with a default target defined from the current
+// system architecture. This prevents hardcoding target in cargo xtask
+// alias, which is not portable
+fn main() -> Result<(), Box<dyn Error>> {
+    let default_target = format!("{}-unknown-linux-gnu", env::consts::ARCH);
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    let status = std::process::Command::new("cargo")
+        .arg("run")
+        .arg("-q")
+        .arg(format!("--target={}", default_target))
+        .arg("--release")
+        .arg("--package")
+        .arg("xtask")
+        .arg("--")
+        .args(args)
+        .status()?;
+
+    if !status.success() {
+        return Err(format!("command failed with status: {}", status).into());
+    }
+
+    Ok(())
+}
