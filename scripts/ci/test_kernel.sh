9ad9e6839837e7541bb12f7092e7ec56dabc9a19
Quentin JEROME <qjerome@rawsec.lu>
2025-02-19 09:08:52 +0100

    add(ci): aarch64 test case (#182)
    * add(ci): aarch64 test case
    
    * add(ci): cache kernels
    
    * change(ci): split build and test
    
    * change: build and cache boths arch
    
    * fix(ci): add libbpf-dev in tests
    
    * fix: missing gcc-aarch64 in tests
    
    * chore: use action cache v4
    
    * rename: tracker job
    
    * fix: gcc-aarch64 installation condition
    
    * change: simplify steps

--- scripts/ci/test_kernel.sh
+++ scripts/ci/test_kernel.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
+
+# currently this test will work with a host running on amd64
+# aarch64 will be emulated and is not optimized to run on a aarch64 host
+
 set -euxo pipefail
 
-cargo xtask build --release -- --bin tests
+system_arch=$(uname -m)
+qemu_arch=${ARCH:-$system_arch}
+
+if [[ "$qemu_arch" == "x86_64" ]]; then
+    arch="amd64"
+elif [[ "$qemu_arch" == "aarch64" ]]; then
+    arch="arm64"
+else
+    echo "unimplemented architecture: $qemu_arch"
+    exit 1
+fi
+
+if [[ "$arch" == "amd64" ]]; then
+    cargo xtask build --release -- --bin tests
+elif [[ "$arch" == "arm64" ]]; then
+    CC=aarch64-linux-gnu-gcc CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc cargo xtask build --release --target aarch64-unknown-linux-gnu
+else
+    echo "cannot compile"
+    exit 1
+fi
 
 tmp_dir=$(mktemp -d)
 kernel="${!#}"
@@ -9,21 +32,20 @@ initramfs=${tmp_dir}/initramfs.img
 
 
 # building initramfs with our test binary as init
-cp target/x86_64-unknown-linux-gnu/release/tests $tmp_dir/init
+cp target/${qemu_arch}-unknown-linux-gnu/release/tests $tmp_dir/init
 echo "init" | cpio -D $tmp_dir -o -H newc > $initramfs
 
 distro="ubuntu"
 while read version
 do
-    base_url=https://kernel.ubuntu.com/mainline/${version}/amd64/
-    image=$(curl -L $base_url | grep -oP 'linux-image-unsigned-.*?-generic.*?amd64.deb' | grep -F "linux-image-unsigned-${kernel}" | tail -n 1  || true)
+    base_url=https://kernel.ubuntu.com/mainline/${version}/${arch}/
+    image=$(curl -L $base_url | grep -oP 'linux-image-unsigned-.*?-generic.*?\.deb' | grep "${arch}.deb" | grep -F "linux-image-unsigned-${kernel}" | tail -n 1  || true)
     if [[ $image ]]
     then
         break
     fi
-done < <(curl https://kernel.ubuntu.com/mainline/ | grep -oP 'href="v\d+\.\d+(\.\d+)?/"' | cut -d '"' -f 2 | tr -d '/' | sort -rV | grep "v${kernel}")
+done < <(curl https://kernel.ubuntu.com/mainline/ | grep -oP 'href="v\d+\.\d+(\.\d+)?/"' | cut -d '"' -f 2 | tr -d '/' | sort -rV | grep -E "v${kernel}($|\.)")
         
-
 # no kernel found
 if [[ ! $image ]]
 then
@@ -31,16 +53,10 @@ then
     exit 1
 fi
 
-if [[ $(echo $@ | grep -c -- '--cache-key') > 0 ]]
-then
-    echo "amd64-vmlinuz-$(md5sum <<<$image | cut -d ' ' -f 1)"
-    exit 0
-fi
-
 precise_version=$(cut -d '_' -f 2 <<<$image)
 
 cache_root="cache/vmlinuz"
-cache_dir="${cache_root}/amd64/${distro}/${image}"
+cache_dir="${cache_root}/${arch}/${distro}/${image}"
 
 if [[ ! -d $cache_dir ]]
 then
@@ -63,7 +79,16 @@ then
     QEMU_ARGS+=(-accel kvm)
 fi
 
-qemu-system-x86_64 -kernel ${cache_dir}/vmlinuz* -m 512m -initrd $initramfs -append "lsm=bpf console=ttyS0" -nographic ${QEMU_ARGS[@]} | tee ${kernel_logs}
+if [[ "$arch" == "amd64" ]]; then
+    qemu-system-x86_64 -smp 4 -kernel ${cache_dir}/vmlinuz* -m 2G -initrd $initramfs -append "lsm=bpf console=ttyS0" -nographic ${QEMU_ARGS[@]} | tee ${kernel_logs}
+elif [[ "$arch" == "arm64" ]]; then
+    # we need to run cortex-a57 otherwise older kernels won't boot
+    # we need to remove console=ttyS0
+    qemu-system-aarch64 -M virt -smp 4 -cpu cortex-a57 -kernel ${cache_dir}/vmlinuz* -m 2G -initrd $initramfs -append "lsm=bpf" -nographic ${QEMU_ARGS[@]} | tee ${kernel_logs}
+else
+    echo "inimplemented arch: $arch"
+    exit 1
+fi
 
 tail -n 30 $kernel_logs | grep 'SUCCESS' > /dev/null
 
