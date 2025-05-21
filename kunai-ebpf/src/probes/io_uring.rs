0a8917f3287739450da1274e776ea6eb1cd8269c
qjerome <qjerome@rawsec.lu>
2025-05-21 16:21:23 +0200

    fix(workflow): kernel probe tracker

--- kunai-ebpf/src/probes/io_uring.rs
+++ kunai-ebpf/src/probes/io_uring.rs
@@ -7,6 +7,13 @@ use kunai_common::co_re::{io_kiocb, sqe_submit};
 // souce code and attach to the caller.
 
 // For kernels in [ 5.5; 6.14 ]
+// match-proto:v5.5:fs/io_uring.c:static int io_issue_sqe(struct io_kiocb *req, const struct io_uring_sqe *sqe, struct io_kiocb **nxt, bool force_nonblock)
+// match-proto:v5.7:fs/io_uring.c:static int io_issue_sqe(struct io_kiocb *req, const struct io_uring_sqe *sqe, bool force_nonblock)
+// match-proto:v5.9:fs/io_uring.c:static int io_issue_sqe(struct io_kiocb *req, const struct io_uring_sqe *sqe, bool force_nonblock, struct io_comp_state *cs)
+// match-proto:v5.10:fs/io_uring.c:static int io_issue_sqe(struct io_kiocb *req, bool force_nonblock, struct io_comp_state *cs)
+// match-proto:v5.12:fs/io_uring.c:static int io_issue_sqe(struct io_kiocb *req, unsigned int issue_flags)
+// match-proto:v6.0:io_uring/io_uring.c:static int io_issue_sqe(struct io_kiocb *req, unsigned int issue_flags)
+// match-proto:v6.15:UNTESTED
 #[kprobe(function = "io_issue_sqe")]
 pub fn enter_io_issue_sqe(ctx: ProbeContext) -> u32 {
     if is_current_loader_task() {
@@ -38,6 +45,7 @@ unsafe fn try_io_issue_sqe(ctx: &ProbeContext) -> ProbeResult<()> {
 }
 
 // For kernels in [5.1; 5.4]
+// match-proto:v5.1:fs/io_uring.c:static int __io_submit_sqe(struct io_ring_ctx *ctx, struct io_kiocb *req, const struct sqe_submit *s, bool force_nonblock)
 #[kprobe(function = "__io_submit_sqe")]
 pub fn enter_io_submit_sqe(ctx: ProbeContext) -> u32 {
     if is_current_loader_task() {
