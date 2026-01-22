e9845f2d6d76cb4c447108329c12dfd2b70de76f
qjerome <qjerome@rawsec.lu>
2026-01-22 13:54:06 +0100

    debug: show xlaunch command

--- xlaunch/src/main.rs
+++ xlaunch/src/main.rs
@@ -7,16 +7,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     let default_target = format!("{}-unknown-linux-gnu", env::consts::ARCH);
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let status = std::process::Command::new("cargo")
-        .arg("run")
+    let mut cmd = std::process::Command::new("cargo");
+
+    cmd.arg("run")
         .arg("-q")
         .arg(format!("--target={}", default_target))
         .arg("--release")
         .arg("--package")
         .arg("xtask")
         .arg("--")
-        .args(args)
-        .status()?;
+        .args(args);
+
+    println!("{cmd:?}");
+
+    let status = cmd.status()?;
 
     if !status.success() {
         return Err(format!("command failed with status: {}", status).into());
