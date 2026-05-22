fecf48ee74ac50ff7ffe570fc32ba5845de209d7
qjerome <qjerome@rawsec.lu>
2026-05-22 08:34:59 +0200

    chore: upgrade yara
    - configure scanner for partial scanning
    - show message on partial scan

--- kunai-common/src/co_re/gen/x86_64.rs
+++ kunai-common/src/co_re/gen/x86_64.rs
@@ -115,7 +115,18 @@ pub struct kuid_t {
     pub val: uid_t,
 }
 #[repr(C)]
-#[derive(Debug, Copy, Clone)]
+#[derive(Copy, Clone)]
+pub struct kernel_cap_t {
+    pub __bindgen_anon_1: kernel_cap_t__bindgen_ty_1,
+}
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub union kernel_cap_t__bindgen_ty_1 {
+    pub cap: [__u32; 2usize],
+    pub val: __u64,
+}
+#[repr(C)]
+#[derive(Copy, Clone)]
 pub struct cred {
     pub uid: kuid_t,
     pub gid: kgid_t,
@@ -125,6 +136,9 @@ pub struct cred {
     pub egid: kgid_t,
     pub fsuid: kuid_t,
     pub fsgid: kgid_t,
+    pub cap_inheritable: kernel_cap_t,
+    pub cap_permitted: kernel_cap_t,
+    pub cap_effective: kernel_cap_t,
 }
 unsafe extern "C" {
     pub fn shim_cred_uid(pcred: *mut cred) -> uid_t;
@@ -149,6 +163,87 @@ unsafe extern "C" {
 }
 unsafe extern "C" {
     pub fn shim_cred_fsgid(pcred: *mut cred) -> gid_t;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_effective_val(cred: *mut cred) -> ::core::ffi::c_ulonglong;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_effective_val_user(cred: *mut cred) -> ::core::ffi::c_ulonglong;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_effective_val_exists(cred: *mut cred) -> bool;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_effective_cap_lo(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_effective_cap_lo_user(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_effective_cap_lo_exists(cred: *mut cred) -> bool;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_effective_cap_hi(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_effective_cap_hi_user(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_effective_cap_hi_exists(cred: *mut cred) -> bool;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_permitted_val(cred: *mut cred) -> ::core::ffi::c_ulonglong;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_permitted_val_user(cred: *mut cred) -> ::core::ffi::c_ulonglong;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_permitted_val_exists(cred: *mut cred) -> bool;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_permitted_cap_lo(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_permitted_cap_lo_user(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_permitted_cap_lo_exists(cred: *mut cred) -> bool;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_permitted_cap_hi(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_permitted_cap_hi_user(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_permitted_cap_hi_exists(cred: *mut cred) -> bool;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_inheritable_val(cred: *mut cred) -> ::core::ffi::c_ulonglong;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_inheritable_val_user(cred: *mut cred) -> ::core::ffi::c_ulonglong;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_inheritable_val_exists(cred: *mut cred) -> bool;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_inheritable_cap_lo(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_inheritable_cap_lo_user(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_inheritable_cap_lo_exists(cred: *mut cred) -> bool;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_inheritable_cap_hi(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_inheritable_cap_hi_user(cred: *mut cred) -> ::core::ffi::c_uint;
+}
+unsafe extern "C" {
+    pub fn shim_cred_cap_inheritable_cap_hi_exists(cred: *mut cred) -> bool;
 }
 #[repr(C)]
 #[derive(Copy, Clone)]
